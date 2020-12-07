8be3baa4d87e760e866e1e033d07f8b51e627596
Miulle <mika.strandb@gmail.com>
2020-12-07 20:24:59 +0100

    added api fetch function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing');
+// Unsplash API
+const count = 10;
+const apiKey = config.MY_KEY;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+
+    }
+}
+
+// On Load
+getPhotos();
